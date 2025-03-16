33c0daa1993fdcef7ec3374caf6d03948ba4f6ec
Phan Tan Duy <tduy13903@gmail.com>
2025-03-16 14:50:06 +0700

    Function calculateShoppingBudget

--- BaiTap1.js
+++ BaiTap1.js
@@ -1,4 +1,4 @@
-list = [[1, 2000], [3, 4000], [5, 6000]];
+list = [[1, 2000], [4, 4000], [5, 6000]];
 function calculateShoppingBudget(list) {
     let sum = 0;
     for (let i = 0; i < list.length; i++) {
